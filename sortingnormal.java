61401868c95f7cfbab07d9ac27e6b3d8330b79fe
ARTHI-dev08 <arthip4505@gmail.com>
2026-07-15 12:28:28 +0530

    added some more medium level ques

--- sortingnormal.java
+++ sortingnormal.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class sortingnormal {
+    static void main() {
+        int arr[]={3,5,87,2,4,6,8};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int tem=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=tem;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
